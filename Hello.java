4f8f381a7f757522c300179efa944df38222a79f
aigbeefe <127797148+aigbeefe@users.noreply.github.com>
2023-05-08 22:01:43 -0600

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
   class Hello {
     public static void main(String[] args) {
-      System.out.println("This is a java app created using Docker and edited for Poll SCM demonstration");
+      System.out.println("This is a java app created using Docker and edited for Poll SCM  project demonstration");
     }
   }
